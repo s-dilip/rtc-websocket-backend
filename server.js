28a87b18063df04f920328874b2851f62cde67cd
Swastik Dilip <justsyd224@gmail.com>
2023-01-31 21:01:20 +0000

    Added message receiving

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const WebSocket = require("ws");
 const server = new WebSocket.Server({ port: 8080 });
 
+// let messages = [];
+
 server.on("connection", (ws) => {
   console.log("New Client Connected");
 
   ws.send("Hello Client!");
+
+  ws.on("message", (message) => {
+    console.log("Recieved: " + message);
+    // messages.push({ id: 0, text: message });
+  });
 });
